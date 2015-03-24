cfbb849cd37ea35beda4580ef4a685a66cedd180
Oguz Huner <oguzcanhuner@gmail.com>
2015-03-24 09:53:12 +0000

    refactor grid

--- models/grid.rb
+++ models/grid.rb
@@ -2,30 +2,36 @@ class Grid
   def initialize(x: 10, y: 10)
     @x = x
     @y = y
+    @cells = cells
   end
 
-  attr_reader :x, :y
-
-  def cells
-    collection = []
-
-    x.times do |x_pos|
-      y.times do |y_pos|
-        collection << Cell.new(x_pos, y_pos)
-      end
-    end
-
-    collection
-  end
+  attr_reader :x, :y, :cells
 
   def draw
     board = ""
     x.times do |x_pos|
       y.times do |y_pos|
-        board << " . "
+        board << cell(x_pos, y_pos).character
       end
       board << "\n"
     end
     board
+  end
+
+  def cell(x, y)
+    cells[[x, y]]
+  end
+
+  private
+  def cells
+    collection = {}
+
+    x.times do |x_pos|
+      y.times do |y_pos|
+        collection[[x_pos, y_pos]] = Cell.new(x_pos, y_pos)
+      end
+    end
+
+    collection
   end
 end
--- spec/models/grid_spec.rb
+++ spec/models/grid_spec.rb
@@ -4,14 +4,9 @@ describe Grid do
   context 'given a 5 by 5 grid' do
     let(:grid) {Grid.new(x: 5, y: 5)}
 
-    describe '#cells' do
-      let(:cells) { grid.cells }
-      it 'is a list' do
-        expect(cells).to be_kind_of Array
-      end
-
-      it 'has a size of 25' do
-        expect(cells.size).to eq 25
+    describe '#cell' do
+      it 'grants access to cells through coordinates' do
+        expect(grid.cell(1,1)).to be_kind_of Cell
       end
     end
 
