1a2b06a3ed59f6a26a2e4f1408c2df82e4758fc7
Oguz Huner <oguzcanhuner@gmail.com>
2015-03-26 22:45:49 +0000

    dont move player when there is nowhere to move

--- lib/mover.rb
+++ lib/mover.rb
@@ -7,8 +7,10 @@ class Mover
   def move(direction)
     current_coords = @moveable.coords
     new_coords = calculate_new_coords(current_coords, direction)
-    @grid.populate_cell(new_coords[0], new_coords[1], @moveable)
-    @grid.empty_cell(current_coords[0], current_coords[1])
+    if @grid.cell(new_coords[0], new_coords[1])
+      @grid.populate_cell(new_coords[0], new_coords[1], @moveable)
+      @grid.empty_cell(current_coords[0], current_coords[1])
+    end
   end
 
   private
--- spec/lib/mover_spec.rb
+++ spec/lib/mover_spec.rb
@@ -1,31 +1,44 @@
 require 'spec_helper'
 
 describe Mover do
-  let(:grid) { Grid.new }
+  let(:grid) { Grid.new(height: 10, width: 10) }
   let(:player) { Player.new }
   let(:mover) { Mover.new(grid, player) }
 
-  before do
-    grid.populate_cell(1, 0, player)
-  end
 
-  context 'given a valid direction' do
-    it 'moves a content in said direction' do
-      expect{ mover.move(:left) }.to change {
-        player.coords
-      }.from([1,0]).to [0,0]
+  describe '#move' do
+    context 'given a valid direction' do
+      before { grid.populate_cell(1, 0, player) }
+
+      it 'moves a content in said direction' do
+        expect{ mover.move(:left) }.to change {
+          player.coords
+        }.from([1,0]).to [0,0]
+      end
+
+      it 'empties the old cell' do
+        expect{ mover.move(:left) }.to change {
+          grid.cell(1, 0).content
+        }.from( player ).to a_kind_of( EmptyCell )
+      end
+
+      it 'populates the new cell' do
+        expect{ mover.move(:left) }.to change {
+          grid.cell(0, 0).content
+        }
+      end
     end
 
-    it 'empties the old cell' do
-      expect{ mover.move(:left) }.to change {
-        grid.cell(1, 0).content
-      }.from( player ).to a_kind_of( EmptyCell )
-    end
+    context 'given that there is no cell in the given direction' do
+      before { grid.populate_cell(0, 0, player) }
 
-    it 'populates the new cell' do
-      expect{ mover.move(:left) }.to change {
-        grid.cell(0, 0).content
-      }
+      it 'does not move the player' do
+        expect{ mover.move(:left) }.not_to change { player.coords }
+      end
+
+      it 'does not modify the grid' do
+        expect{ mover.move(:left) }.not_to change { grid.cell(0, 0).content }
+      end
     end
   end
 end
