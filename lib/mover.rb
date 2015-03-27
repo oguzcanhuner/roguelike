5c74088a12417ee6de6e7af34182367b3d3d8048
Oguz Huner <oguzcanhuner@gmail.com>
2015-03-27 16:50:04 +0000

    add some feature specs for movement

--- lib/mover.rb
+++ lib/mover.rb
@@ -7,9 +7,9 @@ class Mover
   def move(direction)
     current_coords = @moveable.coords
     new_coords = calculate_new_coords(current_coords, direction)
-    if @grid.cell(new_coords[0], new_coords[1])
-      @grid.populate_cell(new_coords[0], new_coords[1], @moveable)
-      @grid.empty_cell(current_coords[0], current_coords[1])
+    if @grid.cell(*new_coords)
+      @grid.populate_cell(*new_coords, @moveable)
+      @grid.empty_cell(*current_coords)
     end
   end
 
---
+++ spec/features/movement_feature_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe 'Cell to cell movement' do
+  let(:grid) { Grid.new(height: 5, width: 6) }
+  let(:player) { Player.new }
+  let(:mover) { Mover.new(grid, player) }
+  before { grid.populate_cell(1, 1, player) }
+
+  let(:original_position) {
+        <<-ORIGNAL
+ .  .  .  .  .  . 
+ .  @  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+        ORIGNAL
+  }
+
+  it 'moves left' do
+    expect{ mover.move(:left) }.to change {
+      grid.draw
+    }.from(
+      original_position
+    ).to(
+        <<-ORIGNAL
+ .  .  .  .  .  . 
+ @  .  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+        ORIGNAL
+    )
+  end
+
+  it 'moves right' do
+    expect{ mover.move(:right) }.to change {
+      grid.draw
+    }.from(
+      original_position
+    ).to(
+        <<-ORIGNAL
+ .  .  .  .  .  . 
+ .  .  @  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+        ORIGNAL
+    )
+  end
+  it 'moves up' do
+    expect{ mover.move(:up) }.to change {
+      grid.draw
+    }.from(
+      original_position
+    ).to(
+        <<-ORIGNAL
+ .  @  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+        ORIGNAL
+    )
+  end
+  it 'moves down' do
+    expect{ mover.move(:down) }.to change {
+      grid.draw
+    }.from(
+      original_position
+    ).to(
+        <<-ORIGNAL
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+ .  @  .  .  .  . 
+ .  .  .  .  .  . 
+ .  .  .  .  .  . 
+        ORIGNAL
+    )
+  end
+end
