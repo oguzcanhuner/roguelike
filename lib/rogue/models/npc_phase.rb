4213be289124da72a0220456b5c08e90c7f8d450
Oguz Huner <oguzcanhuner@gmail.com>
2016-04-20 01:13:41 +0100

    add diagonal movement for npcs

--- lib/rogue/models/npc_phase.rb
+++ lib/rogue/models/npc_phase.rb
@@ -5,8 +5,14 @@ class NpcPhase < Phase
       if npc.coord.adjacent?(@game.player.coord)
         npc.attack(@player)
       else
-        npc.move(direction: [:left, :right, :up, :down].sample)
+        npc.move(direction: random_direction )
       end
     end
+  end
+
+  private
+  def random_direction
+    [:left, :right, :up, :down, :topleft, 
+     :topright, :bottomleft, :bottomright].sample
   end
 end
