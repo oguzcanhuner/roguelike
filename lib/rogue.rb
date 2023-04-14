563fec88a2c389040c7c573b17e0819bed367c64
Darren Coxall <darren.coxall@boardintelligence.com>
2023-04-14 21:00:39 +0000

    Support for reproducible rng seeds

--- lib/rogue.rb
+++ lib/rogue.rb
@@ -4,6 +4,7 @@ require_all 'lib'
 module Rogue
   class << self
     def initialize
+      seed_rng!
       @map = Map.new(height: 100, width: 150)
       map_generator = MapGenerator.new(map: @map)
       map_generator.generate_cave
@@ -11,5 +12,12 @@ module Rogue
       @game = Game.new(map: map)
     end
     attr_reader :map, :game
+
+    private
+
+    def seed_rng!
+      seed = ENV.fetch('RNG_SEED', rand(0..9999))
+      Kernel.srand(Integer(seed))
+    end
   end
 end
